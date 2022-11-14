d82a90d29c2c6db087a551956cdace3d3e28c017
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2022-11-14 15:29:04 +0100

    Update contact.js

--- ui/pages/contact.js
+++ ui/pages/contact.js
@@ -38,27 +38,27 @@ const contact = () => (
               suggestion d’amélioration sur nos outils, contactez nous par email.
             </p>
             <p>
-              <b>Vous êtes candidat,</b> écrivez-nous en cliquant sur <br />
+              <b>Vous êtes candidat,</b> écrivez-nous en cliquant sur ce <br />
               <a href="mailto:labonnealternance@apprentissage.beta.gouv.fr?subject=Page%20Contact%20-%20Candidat">
-                ce lien
+                lien vers notre adresse email
               </a>
             </p>
             <p>
-              <b>Vous êtes un organisme de formation,</b> écrivez-nous en cliquant sur <br />
+              <b>Vous êtes un organisme de formation,</b> écrivez-nous en cliquant sur ce <br />
               <a href="mailto:labonnealternance@apprentissage.beta.gouv.fr?subject=Page%20Contact%20-%20OF">
-                ce lien
+                lien vers notre adresse email
               </a>
             </p>
             <p>
-              <b>Vous êtes une entreprise recevant des candidatures spontanées,</b> écrivez-nous en cliquant sur {" "}
+              <b>Vous êtes une entreprise recevant des candidatures spontanées,</b> écrivez-nous en cliquant sur ce {" "}
               <a href="mailto:labonnealternance@apprentissage.beta.gouv.fr?subject=Page%20Contact%20-%20Entreprise%20-%20Candidature%20spontanée">
-                ce lien
+                lien vers notre adresse email
               </a>
             </p>
             <p>
-              <b>Vous êtes une entreprise intéressée par notre service de dépôt d&apos;offre simplifié,</b> écrivez-nous en cliquant sur {" "}
+              <b>Vous êtes une entreprise intéressée par notre service de dépôt d&apos;offre simplifié,</b> écrivez-nous en cliquant sur ce {" "}
               <a href="mailto:labonnealternance@apprentissage.beta.gouv.fr?subject=Page%20Contact%20-%20Entreprise%20-%20Depot%20offre">
-                ce lien
+                lien vers notre adresse email
               </a>
             </p>
           </div>
