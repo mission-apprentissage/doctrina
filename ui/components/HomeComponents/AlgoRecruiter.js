9d6183967e4dacec4b6b22f2532d9209b50048a3
David Boureau <bdavidxyz@gmail.com>
2022-11-21 14:20:30 +0100

    feat: AlgoRecruiter

--- ui/components/HomeComponents/AlgoRecruiter.js
+++ ui/components/HomeComponents/AlgoRecruiter.js
@@ -1,30 +1,34 @@
 import React from "react"
-import { Divider, Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
+import { Box, Divider, Text, UnorderedList, ListItem } from "@chakra-ui/react"
 
 const AlgoRecruiter = () => {
   return (
     <section className="c-algo container pl-5 py-4 mb-4">
-      <div className="">
-        <h2 className="c-algo-title">
-          <span className="d-block c-algo-title__top">La bonne alternance révèle</span>
-          <span className="d-block c-algo-title__down">le marché caché de l&apos;emploi</span>
-        </h2>
+      <Box>
+        <Box as="h2">
+          <Text as="span" display="block" mb={1} variant="editorialContentH1" color="#2a2a2a">
+            La bonne alternance révèle
+          </Text>
+          <Text as="span" display="block" mb={1} variant="editorialContentH1">
+            le marché caché de l&apos;emploi
+          </Text>
+        </Box>
+
         <Divider variant="pageTitleDivider" my={12} />
-        <p className="c-algo-text">
+        <Box as="p">
           Le saviez-vous ? Afin d&apos;aider les candidats intéressés par l&apos;alternance à trouver un contrat, nous exposons différents types d&apos;entreprises sur notre
           service :
-        </p>
-        <ul className="c-algo-text">
-          <li>
-            <strong>Celles ayant émis un besoin en recrutement </strong>sur notre plateforme ainsi que sur Pôle emploi et ses sites partenaires
-          </li>
-          <li className="pt-3">
+        </Box>
+        <UnorderedList mb="2">
+          <ListItem><strong>Celles ayant émis un besoin en recrutement </strong>sur notre plateforme ainsi que sur Pôle emploi et ses sites partenaires</ListItem>
+          <ListItem>
             <strong>Celles n&apos;ayant pas diffusé d&apos;offres, mais ayant été identifiées comme &quot;à fort potentiel d&apos;embauche en alternance&quot;</strong> par un
             algorithme prédictif de Pôle emploi, qui analyse les recrutements des 6 années passées en CDI, CDD de plus de 30 jours et alternance. L’objectif de cet algorithme est
             de rendre accessible le marché caché de l’emploi, et ainsi faciliter les démarches de candidatures spontanées des usagers du service.
-          </li>
-        </ul>
-      </div>
+          </ListItem>
+          <ListItem>Les présentes conditions d’utilisation peuvent être modifiées à tout moment et la date de mise à jour est mentionnée.</ListItem>
+        </UnorderedList>
+      </Box>
       <div className="c-algo-img d-flex-center">
         <img className="" src="/images/icons/algo_recruiter.svg" alt="" />
       </div>
