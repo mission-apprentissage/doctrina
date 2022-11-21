5f64ac2fe58481ac641cc527092b38d8d0770a00
David Boureau <bdavidxyz@gmail.com>
2022-11-21 11:16:22 +0100

    feat: grid

--- ui/components/HomeComponents/GerezOffres.js
+++ ui/components/HomeComponents/GerezOffres.js
@@ -1,20 +1,32 @@
 import React from "react"
-import { Box, Image, SimpleGrid, Text, Show } from "@chakra-ui/react"
+import { Box, Image, SimpleGrid, Text, Show, Button } from "@chakra-ui/react"
 
 const GerezOffres = () => {
   return (
     <Box as="section" p={3} mb={5}>
-      <Box>
-        <Text className="c-homecomponent-bientot mb-4 d-block d-md-none">Bientôt</Text>
-        <Image src="/images/home_pics/illu-candidatures.svg" alt="" />
-      </Box>
-      <Box className="col-12 col-md-6 mb-5">
-        <Text>Bientôt</Text>
-        <Text as="h2">Gérez vos offres de manière collaborative</Text>
-        <Text>Un accès multi-comptes permettra à plusieurs personnes de votre entreprise d’accéder et de gérer vos offres d&apos;emploi.</Text>
-        <Text as="h2">Consultez et gérez vos candidatures</Text>
-        <Text>Vérifiez d&apos;un coup d&apos;œil la progression des candidatures pour définir les prochaines étapes.</Text>
-      </Box>
+      <SimpleGrid columns={{ sm: 1, md: 2 }} spacing="40px">
+        <Show above="md">
+          <Box>&nbsp;</Box>
+        </Show>
+        <Box>
+          <Button>Bientôt</Button>
+        </Box>
+      </SimpleGrid>
+      <SimpleGrid columns={{ sm: 1, md: 2 }} spacing="40px">
+        <Box>
+          <Image src="/images/home_pics/illu-candidatures.svg" alt="" />
+        </Box>
+        <Box>
+          <Box>
+            <Text as="h2">Gérez vos offres de manière collaborative</Text>
+            <Text>Un accès multi-comptes permettra à plusieurs personnes de votre entreprise d’accéder et de gérer vos offres d&apos;emploi.</Text>
+          </Box>
+          <Box>
+            <Text as="h2">Consultez et gérez vos candidatures</Text>
+            <Text>Vérifiez d&apos;un coup d&apos;œil la progression des candidatures pour définir les prochaines étapes.</Text>
+          </Box>
+        </Box>
+      </SimpleGrid>
     </Box >
   )
 }
