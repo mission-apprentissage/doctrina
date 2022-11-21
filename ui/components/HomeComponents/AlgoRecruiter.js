8d27df7e3151511d9f8befd835fd3ffd1508eb1e
David Boureau <bdavidxyz@gmail.com>
2022-11-21 14:46:10 +0100

    feat: wip gridColumns

--- ui/components/HomeComponents/AlgoRecruiter.js
+++ ui/components/HomeComponents/AlgoRecruiter.js
@@ -1,12 +1,12 @@
 import React from "react"
-import { Box, Divider, Text, UnorderedList, ListItem } from "@chakra-ui/react"
+import { Box, Divider, Text, UnorderedList, ListItem, SimpleGrid } from "@chakra-ui/react"
 
 const AlgoRecruiter = () => {
   return (
     <Box as="section" pb={24} backgroundColor="white">
       <Box py={12} backgroundColor="#f5f5fe">
-        <SimpleGrid columns={{ sm: 1, md: 2 }} spacing="40px">
-          <Box>
+        <SimpleGrid columns={{ sm: 1, lg: 2 }} spacing="20px">
+          <Box pl="12">
             <Box as="h2">
               <Text as="span" display="block" mb={1} variant="editorialContentH1" color="#2a2a2a">
                 La bonne alternance révèle
@@ -17,21 +17,21 @@ const AlgoRecruiter = () => {
             </Box>
 
             <Divider variant="pageTitleDivider" my={12} />
-            <Box as="p">
+            <Box as="p" mb="3">
               Le saviez-vous ? Afin d&apos;aider les candidats intéressés par l&apos;alternance à trouver un contrat, nous exposons différents types d&apos;entreprises sur notre
               service :
             </Box>
-            <UnorderedList mb="2">
-              <ListItem><strong>Celles ayant émis un besoin en recrutement </strong>sur notre plateforme ainsi que sur Pôle emploi et ses sites partenaires</ListItem>
-              <ListItem>
+            <UnorderedList>
+              <ListItem mb="3"><strong>Celles ayant émis un besoin en recrutement </strong>sur notre plateforme ainsi que sur Pôle emploi et ses sites partenaires</ListItem>
+              <ListItem mb="3">
                 <strong>Celles n&apos;ayant pas diffusé d&apos;offres, mais ayant été identifiées comme &quot;à fort potentiel d&apos;embauche en alternance&quot;</strong> par un
                 algorithme prédictif de Pôle emploi, qui analyse les recrutements des 6 années passées en CDI, CDD de plus de 30 jours et alternance. L’objectif de cet algorithme est
                 de rendre accessible le marché caché de l’emploi, et ainsi faciliter les démarches de candidatures spontanées des usagers du service.
               </ListItem>
-              <ListItem>Les présentes conditions d’utilisation peuvent être modifiées à tout moment et la date de mise à jour est mentionnée.</ListItem>
+              <ListItem mb="3">Les présentes conditions d’utilisation peuvent être modifiées à tout moment et la date de mise à jour est mentionnée.</ListItem>
             </UnorderedList>
           </Box>
-          <Box>
+          <Box display="flex" justifyContent="center" alignItems="center">
             <div className="c-algo-img d-flex-center">
               <img className="" src="/images/icons/algo_recruiter.svg" alt="" />
             </div>
