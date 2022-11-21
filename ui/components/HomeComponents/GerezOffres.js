9394665731c90d465a8ac8c8d0a86e55fde1f251
David Boureau <bdavidxyz@gmail.com>
2022-11-21 11:40:32 +0100

    feat: whitebox

--- ui/components/HomeComponents/GerezOffres.js
+++ ui/components/HomeComponents/GerezOffres.js
@@ -3,31 +3,33 @@ import { Box, Image, SimpleGrid, Text, Show, Button } from "@chakra-ui/react"
 
 const GerezOffres = () => {
   return (
-    <Box as="section" p={3} mb={5}>
-      <SimpleGrid columns={{ sm: 1, md: 2 }} spacing="40px">
-        <Show above="md">
-          <Box>&nbsp;</Box>
-        </Show>
-        <Box>
-          <Box as="span" background="linear-gradient(90deg,#6a11cb,#2575fc)" color="#fff" borderRadius="80" lineHeight="32px" px={9} py={2} fontSize="20px" fontWeight="700">Bientôt</Box>
-        </Box>
-      </SimpleGrid>
-      <Box>&nbsp;</Box>
-      <SimpleGrid columns={{ sm: 1, md: 2 }} spacing="40px">
-        <Box>
-          <Image src="/images/home_pics/illu-candidatures.svg" alt="" />
-        </Box>
-        <Box>
+    <Box as="section" py={24} backgroundColor="white">
+      <Box backgroundColor="#f8f8f8">
+        <SimpleGrid columns={{ sm: 1, md: 2 }} spacing="40px">
+          <Show above="md">
+            <Box>&nbsp;</Box>
+          </Show>
           <Box>
-            <Text as="h2">Gérez vos offres de manière collaborative</Text>
-            <Text>Un accès multi-comptes permettra à plusieurs personnes de votre entreprise d’accéder et de gérer vos offres d&apos;emploi.</Text>
+            <Box as="span" background="linear-gradient(90deg,#6a11cb,#2575fc)" color="#fff" borderRadius="80" lineHeight="32px" px={9} py={2} fontSize="20px" fontWeight="700">Bientôt</Box>
+          </Box>
+        </SimpleGrid>
+        <Box>&nbsp;</Box>
+        <SimpleGrid columns={{ sm: 1, md: 2 }} spacing="40px">
+          <Box>
+            <Image src="/images/home_pics/illu-candidatures.svg" alt="" />
           </Box>
           <Box>
-            <Text as="h2">Consultez et gérez vos candidatures</Text>
-            <Text>Vérifiez d&apos;un coup d&apos;œil la progression des candidatures pour définir les prochaines étapes.</Text>
+            <Box>
+              <Text as="h2">Gérez vos offres de manière collaborative</Text>
+              <Text>Un accès multi-comptes permettra à plusieurs personnes de votre entreprise d’accéder et de gérer vos offres d&apos;emploi.</Text>
+            </Box>
+            <Box>
+              <Text as="h2">Consultez et gérez vos candidatures</Text>
+              <Text>Vérifiez d&apos;un coup d&apos;œil la progression des candidatures pour définir les prochaines étapes.</Text>
+            </Box>
           </Box>
-        </Box>
-      </SimpleGrid>
+        </SimpleGrid>
+      </Box >
     </Box >
   )
 }
