407453c0d97ea4d43a4e5d10307ed0b19ff2115c
David Boureau <bdavidxyz@gmail.com>
2022-11-22 17:00:17 +0100

    feat: HomeHero done

--- ui/components/HomeHero/HomeHero.js
+++ ui/components/HomeHero/HomeHero.js
@@ -5,7 +5,7 @@ import { Container, Box } from "@chakra-ui/react"
 const HomeHero = () => {
   return (
     <Box backgroundSize="contain" backgroundRepeat="no-repeat">
-      <Container variant="responsiveContainer" pt={{ base: 3, sm: 12 }} pb={{ base: 0, sm: 12 }}>
+      <Container variant="responsiveContainer" pt={{ base: 3, sm: 12 }} pb={{ base: 0, sm: 12 }} position="relative">
         <Box boxShadow="0 4px 12px 2px rgb(0 0 0 / 21%)" pt="6" pb="10" bg="#fff" backgroundClip="border-box" borderRadius="10px">
           <StartForm />
         </Box>
