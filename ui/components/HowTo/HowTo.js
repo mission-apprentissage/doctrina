8ad1151419d87b60a995c6ffbfd3fa3080d1402e
David Boureau <bdavidxyz@gmail.com>
2022-11-22 15:36:40 +0100

    feat: 2nd griditem

--- ui/components/HowTo/HowTo.js
+++ ui/components/HowTo/HowTo.js
@@ -6,30 +6,34 @@ import howtoline1 from "../../public/images/howtoline1.svg"
 import howtoline2a from "../../public/images/howtoline2a.svg"
 import howtoline3a from "../../public/images/howtoline3a.svg"
 import howtoline3b from "../../public/images/howtoline3b.svg"
-import { Image, Text, UnorderedList, ListItem, Divider, GridItem, Grid, Container } from "@chakra-ui/react"
+import { Image, Text, UnorderedList, ListItem, Divider, GridItem, Grid, Container, Box, Show, Hide } from "@chakra-ui/react"
 
 const HowTo = () => {
   return (
     <>
       <Container variant="responsiveContainer">
-        <div className="c-howto-cards card-deck">
-          <div className="card border-0 position-relative">
-            <img src={howto1} className="card-img-top" alt="" />
-            <img src={howtoline1} className="position-absolute c-homebg-img c-homebg-img--line c-homebg-img--line1" alt="" />
-            <div className="card-body">
-              <h5 className="card-title">Le job de vos rêves</h5>
-              <p className="card-text">Renseignez le métier que vous souhaitez faire et la localisation (Ville ou Code postal)</p>
-            </div>
-          </div>
-          <div className="card border-0  position-relative">
-            <img src={howto2} className="card-img-top" alt="" />
-            <img src={howtoline2a} className="position-absolute c-homebg-img c-homebg-img--line c-homebg-img--line2a" alt="" />
-            <div className="card-body">
-              <h5 className="card-title">En un clin d’&oelig;il</h5>
-              <p className="card-text">Obtenez la liste des formations et entreprises proches de chez vous dans le domaine recherché.</p>
-            </div>
-          </div>
-          <div className="card border-0  position-relative">
+        <Grid templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }} gap="24">
+          <GridItem position="relative" width={{ base: "200px", md: "auto" }} mx="auto" mt={{base: "12", md: "0" }}>
+            <Image src={howto1} alt=""  />
+            <Show above="md">
+              <Image src={howtoline1} alt="" position="absolute" top="85px" left="-162px" />
+            </Show>
+            <Box>
+              <Text as="h3">Le job de vos rêves</Text>
+              <Text as="p">Renseignez le métier que vous souhaitez faire et la localisation (Ville ou Code postal)</Text>
+            </Box>
+          </GridItem>
+          <GridItem position="relative" width={{ base: "200px", md: "auto" }} mx="auto" mt={{base: "12", md: "0" }}>
+            <Image src={howto2} alt=""  />
+            <Show above="md">
+              <Image src={howtoline2a} alt="" position="absolute" top="47px" left="-208px" />
+            </Show>
+            <Box>
+              <Text as="h3">En un clin d’&oelig;il</Text>
+              <Text as="p">Obtenez la liste des formations et entreprises proches de chez vous dans le domaine recherché.</Text>
+            </Box>
+          </GridItem>
+          <GridItem>
             <img src={howto3} className="card-img-top" alt="" />
             <img src={howtoline3a} className="position-absolute c-homebg-img c-homebg-img--line c-homebg-img--line3a" alt="" />
             <img src={howtoline3b} className="position-absolute c-homebg-img c-homebg-img--line c-homebg-img--line3b" alt="" />
@@ -37,8 +41,8 @@ const HowTo = () => {
               <h5 className="card-title">Un contact facile</h5>
               <p className="card-text">Contactez facilement les centres de formation ou les entreprises pour postuler </p>
             </div>
-          </div>
-        </div>
+          </GridItem>
+        </Grid>
       </Container>
     </>
   )
