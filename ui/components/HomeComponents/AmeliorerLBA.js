585be8b89167d3965098e4c7d45cf93d55a982f1
David Boureau <bdavidxyz@gmail.com>
2022-11-22 14:09:10 +0100

    feat: AmeliorerLBA

--- ui/components/HomeComponents/AmeliorerLBA.js
+++ ui/components/HomeComponents/AmeliorerLBA.js
@@ -1,32 +1,36 @@
 import React from "react"
-import ExternalLink from "../externalLink"
-import { Box } from "@chakra-ui/react"
+import { Box, Grid, GridItem, Link } from "@chakra-ui/react"
 
 const AmeliorerLBA = () => {
   return (
     <Box as="section" p="3" mb="5">
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <img className="c-homecomponent-illustration mr-3 my-3" src="/images/home_pics/illu-support.svg" alt="" />
-        </div>
-        <div className="col-12 col-md-6 mb-5">
-          <div className="font-weight-bold mb-2">Donnez votre avis</div>
-          <h2 className="c-homecomponent-title__blue mb-4">Aidez-nous à améliorer La bonne alternance</h2>
-          La bonne alternance est un service en construction. Pour le faire évoluer, nous interrogeons régulièrement les utilisateurs du service.
-          <div className="font-weight-bold">
-            Nous vous invitions à participer à un échange en visio d’une trentaine de minutes avec un membre de notre équipe pour répondre à quelques questions et nous partager
-            votre avis.
-          </div>
-          <div className="mt-3">
-            <ExternalLink
-              className="c-homecomponent-link c-homecomponent-link__clear mt-3"
+      <Grid templateColumns="repeat(12, 1fr)">
+        <GridItem>
+          <Image src="/images/home_pics/illu-support.svg" alt="" />
+        </GridItem>
+        <GridItem>
+          <Text>Donnez votre avis</Text>
+          <Text as="h2">Aidez-nous à améliorer La bonne alternance</Text>
+          <Text>
+            La bonne alternance est un service en construction. Pour le faire évoluer, nous interrogeons régulièrement les utilisateurs du service.
+          </Text>
+          <Text>
+            <strong>Nous vous invitions à participer à un échange en visio d’une trentaine de minutes avec un membre de notre équipe pour répondre à quelques questions et nous partager
+              votre avis.</strong>
+          </Text>
+          <Box mt="3">
+            <Link
+              href="https://calendly.com/rdv-labonnealternance/discussion-labonnealternance"
               aria-label="Planifier un échange avec l'équipe"
-              url="https://calendly.com/rdv-labonnealternance/discussion-labonnealternance"
               title="Je participe à l'étude"
-            />
-          </div>
-        </div>
-      </div>
+              isExternal
+            >Je participe à l'étude</Link>
+          </Box>
+          <Box mt="3">
+            &nbsp;
+          </Box>
+        </GridItem>
+      </Grid>
     </Box>
   )
 }
