17024ac05b3cc25072ab8d42442b9f24542600d1
David Boureau <bdavidxyz@gmail.com>
2022-11-22 15:11:51 +0100

    feat: AlgoHome done

--- ui/components/HomeComponents/AlgoHome.js
+++ ui/components/HomeComponents/AlgoHome.js
@@ -1,7 +1,7 @@
 import React from "react"
 import TagCandidatureSpontanee from "../../components/ItemDetail/TagCandidatureSpontanee.js"
 import TagOffreEmploi from "../../components/ItemDetail/TagOffreEmploi.js"
-import { Box, Image, Text, UnorderedList, ListItem, Divider, GridItem, Grid, Container } from "@chakra-ui/react"
+import { Image, Text, UnorderedList, ListItem, Divider, GridItem, Grid, Container } from "@chakra-ui/react"
 
 const AlgoHome = () => {
   return (
@@ -17,16 +17,20 @@ const AlgoHome = () => {
             </Text>
           </Text>
           <Divider variant="pageTitleDivider" my={12} />
-          <Text as="p" className="c-algo-text">La bonne alternance expose différents types d&apos;opportunités d&apos;emplois :</Text>
+          <Text as="p" fontSize="18px" mb="5">La bonne alternance expose différents types d&apos;opportunités d&apos;emplois :</Text>
           <UnorderedList>
             <ListItem>
-              <strong>Les offres d&apos;emploi</strong> : publiées sur notre plateforme ainsi que celles issues de Pôle emploi et ses partenaires. Elles sont identifiées grâce au tag{" "}
-              <TagOffreEmploi />
+              <Text as="p" fontSize="18px" mb="5">
+                <strong>Les offres d&apos;emploi</strong> : publiées sur notre plateforme ainsi que celles issues de Pôle emploi et ses partenaires. Elles sont identifiées grâce au tag{" "}
+                <TagOffreEmploi />
+              </Text>
             </ListItem>
             <ListItem>
-              <strong>Les candidatures spontanées</strong> : correspondant au marché caché de l&apos;emploi. Chaque mois, un algorithme prédictif de Pôle emploi analyse les
-              recrutements des 6 années passées pour prédire ceux des 6 mois à venir. Grâce à ces données, il identifie une liste restreinte d&apos;entreprises &quot;à fort potentiel
-              d&apos;embauche en alternance&quot; pour faciliter vos démarches de candidatures spontanées. Elles sont identifiées grâce au tag <TagCandidatureSpontanee />
+              <Text as="p" fontSize="18px" mb="5">
+                <strong>Les candidatures spontanées</strong> : correspondant au marché caché de l&apos;emploi. Chaque mois, un algorithme prédictif de Pôle emploi analyse les
+                recrutements des 6 années passées pour prédire ceux des 6 mois à venir. Grâce à ces données, il identifie une liste restreinte d&apos;entreprises &quot;à fort potentiel
+                d&apos;embauche en alternance&quot; pour faciliter vos démarches de candidatures spontanées. Elles sont identifiées grâce au tag <TagCandidatureSpontanee />
+              </Text>
             </ListItem>
           </UnorderedList>
         </GridItem>
