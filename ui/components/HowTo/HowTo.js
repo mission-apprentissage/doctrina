9c18c4db4c1f80124bbefab9541c696d53edb89f
David Boureau <bdavidxyz@gmail.com>
2022-11-22 16:18:12 +0100

    style: subtitles

--- ui/components/HowTo/HowTo.js
+++ ui/components/HowTo/HowTo.js
@@ -13,34 +13,36 @@ const HowTo = () => {
     <>
       <Container variant="responsiveContainer">
         <Grid templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }} gap="24">
-          <GridItem position="relative" width={{ base: "200px", md: "auto" }} mx="auto" mt={{base: "12", md: "0" }}>
-            <Image src={howto1} alt=""  />
+          <GridItem position="relative" width={{ base: "200px", md: "auto" }} mx="auto" mt={{base: "12", md: "0" }} mb="12">
+            <Image src={howto1} alt="" />
             <Show above="md">
-              <Image src={howtoline1} alt="" position="absolute" top="85px" left="-162px" />
+              <Image src={howtoline1} alt="" position="absolute" top="85px" left="-162px" zIndex="-1" />
             </Show>
             <Box>
-              <Text as="h3">Le job de vos rêves</Text>
+              <Text as="h3" fontSize="1.25rem" fontWeight="500" mb="3">Le job de vos rêves</Text>
               <Text as="p">Renseignez le métier que vous souhaitez faire et la localisation (Ville ou Code postal)</Text>
             </Box>
           </GridItem>
-          <GridItem position="relative" width={{ base: "200px", md: "auto" }} mx="auto" mt={{base: "12", md: "0" }}>
+          <GridItem position="relative" width={{ base: "200px", md: "auto" }} mx="auto" mt={{base: "12", md: "0" }} mb="12">
             <Image src={howto2} alt=""  />
             <Show above="md">
-              <Image src={howtoline2a} alt="" position="absolute" top="47px" left="-208px" />
+              <Image src={howtoline2a} alt="" position="absolute" top="47px" left="-208px" zIndex="-1" />
             </Show>
             <Box>
-              <Text as="h3">En un clin d’&oelig;il</Text>
+              <Text as="h3" fontSize="1.25rem" fontWeight="500" mb="3">En un clin d’&oelig;il</Text>
               <Text as="p">Obtenez la liste des formations et entreprises proches de chez vous dans le domaine recherché.</Text>
             </Box>
           </GridItem>
-          <GridItem>
-            <img src={howto3} className="card-img-top" alt="" />
-            <img src={howtoline3a} className="position-absolute c-homebg-img c-homebg-img--line c-homebg-img--line3a" alt="" />
-            <img src={howtoline3b} className="position-absolute c-homebg-img c-homebg-img--line c-homebg-img--line3b" alt="" />
-            <div className="card-body">
-              <h5 className="card-title">Un contact facile</h5>
-              <p className="card-text">Contactez facilement les centres de formation ou les entreprises pour postuler </p>
-            </div>
+          <GridItem position="relative" width={{ base: "200px", md: "auto" }} mx="auto" mt={{base: "12", md: "0" }} mb="12">
+            <Image src={howto3} alt=""  />
+            <Show above="md">
+              <Image src={howtoline3a} alt="" position="absolute" top="47px" left="-208px" zIndex="-1" />
+              <Image src={howtoline3b} alt="" position="absolute" top="47px" left="158px" zIndex="-1" />
+            </Show>
+            <Box>
+              <Text as="h3" fontSize="1.25rem" fontWeight="500" mb="3">Un contact facile</Text>
+              <Text as="p">Contactez facilement les centres de formation ou les entreprises pour postuler </Text>
+            </Box>
           </GridItem>
         </Grid>
       </Container>
