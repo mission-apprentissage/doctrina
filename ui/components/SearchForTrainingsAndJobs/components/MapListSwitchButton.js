a9339b2b5cbd8ba92742ecb1aac53673ec8e4de1
Alan Le Ruyet <alan.leruyet@free.fr>
2022-11-23 10:06:08 +0100

    feat: progress

--- ui/components/SearchForTrainingsAndJobs/components/MapListSwitchButton.js
+++ ui/components/SearchForTrainingsAndJobs/components/MapListSwitchButton.js
@@ -3,34 +3,82 @@ import toggleList from "../../../public/images/icons/toggleList.svg"
 import toggleCard from "../../../public/images/icons/toggleCard.svg"
 import { SearchResultContext } from "../../../context/SearchResultContextProvider"
 import { DisplayContext } from "../../../context/DisplayContextProvider"
+import { Box, Button, Image, Text } from "@chakra-ui/react"
 
 const MapListSwitchButton = ({ showResultMap, showResultList, isFormVisible }) => {
   const { hasSearch } = React.useContext(SearchResultContext)
   const { visiblePane } = React.useContext(DisplayContext)
 
+  /*
+
+.demoPage .floatingButtons button {
+  cursor: pointer;
+  margin-right: 30px;
+  background-color: $gray-750;
+  font-size: 14px;
+  color: #fff;
+  border-radius: 58px;
+  border: none;
+  padding: 10px 20px;
+  margin-top: -2rem;
+}
+.demoPage .floatingButtons button:focus {
+  box-shadow: none;
+}
+
+
+.widgetTestPage .floatingButtons button {
+  cursor: pointer;
+  margin-right: 30px;
+  background-color: #0061df;
+  font-size: 14px;
+  color: #fff;
+  border-radius: 19px;
+  border: none;
+  padding: 10px 20px;
+}
+.widgetTestPage .floatingButtons button:focus {
+  box-shadow: none;
+
+  */
+
+  const floatingButtonProperties = {
+    position: "fixed",
+    bottom: ["2.5rem", "1.5rem"],
+    left: "50%",
+    right: ["unset", "40%"],
+    transform: "translateX(-40%)",
+  }
+  const resultListFloatingButtonDisplayProperty = isFormVisible ? "none" : ["block", "block", "none"]
+  const mapFloatingButtonDisplayProperty = ["block", "block", "none"]
+
   if (visiblePane === "resultList") {
     if (hasSearch)
       return (
-        <div className={`floatingButtons is-hidden-${isFormVisible} resultList`}>
-          <button onClick={showResultMap} className="d-flex align-items-center">
-            <img src={toggleCard} alt="Basculer vers la carte" />
-            <span className="ml-2 c-resultlist-card">Carte</span>
-          </button>
-        </div>
+        <Box display={resultListFloatingButtonDisplayProperty} {...floatingButtonProperties}>
+          <Button onClick={showResultMap} className="d-flex align-items-center">
+            <Image src={toggleCard} alt="Basculer vers la carte" />
+            <Text as="span" className="ml-2 c-resultlist-card">
+              Carte
+            </Text>
+          </Button>
+        </Box>
       )
     else return ""
   } else {
     return (
-      <div className="floatingButtons resultMap">
+      <Box display={mapFloatingButtonDisplayProperty} {...floatingButtonProperties}>
         {hasSearch ? (
-          <button onClick={showResultList} className="d-flex align-items-center">
-            <img src={toggleList} alt="Basculer vers la liste" />
-            <span className="ml-2 c-resultlist-card">Liste</span>
-          </button>
+          <Button onClick={showResultList} className="d-flex align-items-center">
+            <Image src={toggleList} alt="Basculer vers la liste" />
+            <Text as="span" className="ml-2 c-resultlist-card">
+              Liste
+            </Text>
+          </Button>
         ) : (
           ""
         )}
-      </div>
+      </Box>
     )
   }
 }
