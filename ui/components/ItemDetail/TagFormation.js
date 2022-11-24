7e0620deebbdb7d5625a8d9dc142f465e93bb674
Alan Le Ruyet <alan.leruyet@free.fr>
2022-11-24 18:08:24 +0100

    feat: tag formation

--- ui/components/ItemDetail/TagFormation.js
+++ ui/components/ItemDetail/TagFormation.js
@@ -1,17 +1,47 @@
 import React from "react"
 
 import bookIcon from "public/images/book.svg"
+import { Flex, Image, Text } from "@chakra-ui/react"
+
+/*
+.c-media-tag.c-media-tag--2nd {
+  margin-left: 0px;
+  margin-top: 3px;
+  margin-right: 4px;
+
+  @include media-breakpoint-up(lg) {
+    margin-left: 4px;
+    margin-right: 0px;
+  }
+}
+
+
+  */
+
+const tagProperties = {
+  fontSize: "12px",
+  fontWeight: 700,
+  display: "inline-flex",
+  lineHeight: "15px",
+  letterSpacing: "0px",
+  margin: "3px 4px 0 0",
+  width: "fit-content",
+  borderRadius: "6px",
+  padding: "0.5rem",
+  paddingTop: "0.25rem",
+  alignItems: "center",
+  color: "greensoft.600",
+  background: "greensoft.200",
+}
 
 const TagFormation = () => {
   return (
-    <>
-      {
-        <span className="c-media-tag c-media-tag--2nd c-media-tag--smiley">
-          <img src={bookIcon} alt="" />
-          <span className="ml-1">Formation</span>
-        </span>
-      }
-    </>
+    <Text as="span" {...tagProperties}>
+      <Image width="16px" mb="-2px" src={bookIcon} alt="" />
+      <Text whiteSpace="nowrap" as="span" ml={1}>
+        Formation
+      </Text>
+    </Text>
   )
 }
 
