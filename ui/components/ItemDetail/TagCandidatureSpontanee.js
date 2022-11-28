bdb327c3549ca2ef9ae2e2ceb4788507aedbefb9
Alan Le Ruyet <alan.leruyet@free.fr>
2022-11-28 11:35:57 +0100

    feat: use variant

--- ui/components/ItemDetail/TagCandidatureSpontanee.js
+++ ui/components/ItemDetail/TagCandidatureSpontanee.js
@@ -4,24 +4,13 @@ import React from "react"
 import paperplaneIcon from "../../public/images/paperplane.svg"
 
 const tagProperties = {
-  fontSize: "12px",
-  fontWeight: 700,
-  display: "inline-flex",
-  lineHeight: "15px",
-  letterSpacing: "0px",
-  margin: "3px 4px 0 0",
-  width: "max-content",
-  borderRadius: "6px",
-  padding: "0.5rem",
-  paddingTop: "0.25rem",
-  alignItems: "center",
   color: "pinksoft.600",
   background: "pinksoft.200",
 }
 
 const TagCandidatureSpontanee = () => {
   return (
-    <Text as="span" {...tagProperties}>
+    <Text as="span" variant="tag" {...tagProperties}>
       <Image width="16px" mb="-2px" src={paperplaneIcon} alt="" />
       <Text whiteSpace="nowrap" as="span" ml={1}>
         Candidature spontanée
--- ui/components/ItemDetail/TagFormation.js
+++ ui/components/ItemDetail/TagFormation.js
@@ -3,24 +3,13 @@ import bookIcon from "public/images/book.svg"
 import { Image, Text } from "@chakra-ui/react"
 
 const tagProperties = {
-  fontSize: "12px",
-  fontWeight: 700,
-  display: "inline-flex",
-  lineHeight: "15px",
-  letterSpacing: "0px",
-  margin: "3px 4px 0 0",
-  width: "max-content",
-  borderRadius: "6px",
-  padding: "0.5rem",
-  paddingTop: "0.25rem",
-  alignItems: "center",
   color: "greensoft.600",
   background: "greensoft.200",
 }
 
 const TagFormation = () => {
   return (
-    <Text as="span" {...tagProperties}>
+    <Text as="span" variant="tag" {...tagProperties}>
       <Image width="16px" mb="-2px" src={bookIcon} alt="" />
       <Text whiteSpace="nowrap" as="span" ml={1}>
         Formation
