cbbd9acae79a62a35a4dfd79890e9ea99d6d6686
David Boureau <bdavidxyz@gmail.com>
2022-11-28 14:43:39 +0100

    style: divider

--- ui/components/footer.js
+++ ui/components/footer.js
@@ -1,116 +1,120 @@
 import { useRouter } from "next/router"
 import React from "react"
-import { Box, Image, GridItem, Grid, Container, Link, UnorderedList, ListItem, Flex } from "@chakra-ui/react"
+import { Box, Image, GridItem, Grid, Container, Link, UnorderedList, ListItem, Flex, Divider } from "@chakra-ui/react"
 
 const Footer = (props) => {
   const router = useRouter()
 
   return (
-    <Box as="footer" maxWidth="1310px" mx="auto">
-      <Box as="nav" pl={{ base: 8, lg: 0 }}>
-        <Grid templateColumns={{ base: "1fr", lg: "repeat(4, 1fr)" }}>
-          <GridItem colSpan={{ base: 1, lg: 1 }}>
-            <Image src="/images/marianne.svg#svgView(viewBox(12 0 162 78))" alt="" width="290" height="130" />
-          </GridItem>
-          <GridItem colSpan={{ base: 1, lg: 1 }}>
-            <Box pl={{ base: 0, lg: 4 }} height="100%" display="flex" alignItems="center">
-              <Image src="/images/france_relance.svg" alt="" width="81" height="81" />
-            </Box>
-          </GridItem>
-          <GridItem colSpan={{ base: 1, lg: 2 }}>
-            <Box pl={{ base: 0, lg: 8 }}>
-              <Box fontSize="14px" color="#1e1e1e">
-                La bonne alternance. Trouvez votre alternance.
+    <Box as="footer">
+      <Box maxWidth="1310px" mx="auto">
+        <Box as="nav" pl={{ base: 8, lg: 0 }}>
+          <Grid templateColumns={{ base: "1fr", lg: "repeat(4, 1fr)" }}>
+            <GridItem colSpan={{ base: 1, lg: 1 }}>
+              <Image src="/images/marianne.svg#svgView(viewBox(12 0 162 78))" alt="" width="290" height="130" />
+            </GridItem>
+            <GridItem colSpan={{ base: 1, lg: 1 }}>
+              <Box pl={{ base: 0, lg: 4 }} height="100%" display="flex" alignItems="center">
+                <Image src="/images/france_relance.svg" alt="" width="81" height="81" />
               </Box>
-              <Box fontSize="14px" color="#1e1e1e" mt="6">
-                La bonne alternance est proposée par les services suivants :
+            </GridItem>
+            <GridItem colSpan={{ base: 1, lg: 2 }}>
+              <Box pl={{ base: 0, lg: 8 }}>
+                <Box fontSize="14px" color="#1e1e1e">
+                  La bonne alternance. Trouvez votre alternance.
+                </Box>
+                <Box fontSize="14px" color="#1e1e1e" mt="6">
+                  La bonne alternance est proposée par les services suivants :
+                </Box>
+                <Flex flexDirection={{ base: "column", lg: "row" }} mt="6">
+                  <Link href="https://pole-emploi.fr" aria-label="Accès au site de Pôle emploi" isExternal fontSize={14} fontWeight={700} color="#666" mr={4}>
+                    pole-emploi.fr
+                  </Link>
+                  <Link href="https://gouvernement.fr" aria-label="Accès au site gouvernement.fr" isExternal fontSize={14} fontWeight={700} color="#666" mr={4}>
+                    gouvernement.fr
+                  </Link>
+                  <Link href="https://service-public.fr" aria-label="Accès au site service-public.fr" isExternal fontSize={14} fontWeight={700} color="#666" mr={4}>
+                    service-public.fr
+                  </Link>
+                  <Link href="https://data.gouv.fr" aria-label="Accès au site data.gouv" isExternal fontSize={14} fontWeight={700} color="#666" mr={4}>
+                    data.gouv.fr
+                  </Link>
+                </Flex>
               </Box>
-              <Flex flexDirection={{ base: "column", lg: "row" }} mt="6">
-                <Link href="https://pole-emploi.fr" aria-label="Accès au site de Pôle emploi" isExternal fontSize={14} fontWeight={700} color="#666" mr={4}>
-                  pole-emploi.fr
-                </Link>
-                <Link href="https://gouvernement.fr" aria-label="Accès au site gouvernement.fr" isExternal fontSize={14} fontWeight={700} color="#666" mr={4}>
-                  gouvernement.fr
-                </Link>
-                <Link href="https://service-public.fr" aria-label="Accès au site service-public.fr" isExternal fontSize={14} fontWeight={700} color="#666" mr={4}>
-                  service-public.fr
-                </Link>
-                <Link href="https://data.gouv.fr" aria-label="Accès au site data.gouv" isExternal fontSize={14} fontWeight={700} color="#666" mr={4}>
-                  data.gouv.fr
-                </Link>
-              </Flex>
-            </Box>
-          </GridItem>
-        </Grid>
+            </GridItem>
+          </Grid>
+        </Box>
       </Box>
-      <Box as="nav">
-        <Container>
+      <Divider my={6}></Divider>
+      <Box maxWidth="1310px" mx="auto">
+        <Box as="nav" pl={{ base: 8, lg: 0 }}>
           <Grid>
             <GridItem>
-              <UnorderedList>
-                <ListItem>
+              <UnorderedList listStyleType="none" color="#666" fontSize={12} marginInlineStart={0}>
+                <ListItem mt={{ base: 3, lg: 0 }}>
                   <Link href="/mentions-legales">
                     <a aria-label="Accès aux mentions légales">Mentions légales</a>
                   </Link>
                 </ListItem>
-                <ListItem>
+                <ListItem mt={{ base: 3, lg: 0 }}>
                   <Link href="/cgu" aria-label="Accès aux conditions générales d'utilisation">
                     CGU
                   </Link>
                 </ListItem>
-                <ListItem>
+                <ListItem mt={{ base: 3, lg: 0 }}>
                   <Link href="/cookies" aria-label="Accès à la page Cookies">
                     Cookies
                   </Link>
                 </ListItem>
-                <ListItem>
+                <ListItem mt={{ base: 3, lg: 0 }}>
                   <Link href="/stats" aria-label="Accès aux statistiques du service">
                     Statistiques
                   </Link>
                 </ListItem>
-                <ListItem>
+                <ListItem mt={{ base: 3, lg: 0 }}>
                   <Link href="/faq" aria-label="Accès à la foire aux questions">
                     FAQ
                   </Link>
                 </ListItem>
-                <ListItem>
+                <ListItem mt={{ base: 3, lg: 0 }}>
                   <Link href="/contact" aria-label="Accès à la page Contact">
                     Contact
                   </Link>
                 </ListItem>
-                <ListItem>
+                <ListItem mt={{ base: 3, lg: 0 }}>
                   <Link href="/metiers" aria-label="Accès à la page Métiers">
                     Métiers
                   </Link>
                 </ListItem>
-                <ListItem>
+                <ListItem mt={{ base: 3, lg: 0 }}>
                   <Link href="/a-propos" aria-label="Accès à la page A propos">
                     A propos
                   </Link>
                 </ListItem>
-                <ListItem>
+                <ListItem mt={{ base: 3, lg: 0 }}>
                   <Link href="/developpeurs" aria-label="Accès à la page Développeurs">
                     Développeurs
                   </Link>
                 </ListItem>
-                <ListItem>
+                <ListItem mt={{ base: 3, lg: 0 }}>
                   <Link>Accessibilité : non conforme</Link>
                 </ListItem>
               </UnorderedList>
             </GridItem>
             <GridItem>
-              <Box>
+              <Box color="#666" fontSize={12}>
                 Sauf mention contraire, tous les contenus de ce site sont sous licence{" "}
                 <Link
-                  url="https://www.etalab.gouv.fr/licence-version-2-0-de-la-licence-ouverte-suite-a-la-consultation-et-presentation-du-decret"
+                  href="https://www.etalab.gouv.fr/licence-version-2-0-de-la-licence-ouverte-suite-a-la-consultation-et-presentation-du-decret"
                   aria-label="Accès au site Etalab"
-                  title="etalab-2.0"
                   withPic={<img src="/images/square_link.svg" alt="Ouverture dans un nouvel onglet" />}
-                />
+                >
+                  etalab-2.0
+                </Link>
               </Box>
             </GridItem>
           </Grid>
-        </Container>
+        </Box>
       </Box>
     </Box>
   )
