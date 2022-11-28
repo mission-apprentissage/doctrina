aacdcc225038534d855e30fed66bb48daf052a01
Alan Le Ruyet <alan.leruyet@free.fr>
2022-11-28 10:53:08 +0100

    feat: tagcandidaturespontanee

--- ui/components/ItemDetail/TagCandidatureSpontanee.js
+++ ui/components/ItemDetail/TagCandidatureSpontanee.js
@@ -1,13 +1,32 @@
+import { Image, Text } from "@chakra-ui/react"
 import React from "react"
 
 import paperplaneIcon from "../../public/images/paperplane.svg"
 
+const tagProperties = {
+  fontSize: "12px",
+  fontWeight: 700,
+  display: "inline-flex",
+  lineHeight: "15px",
+  letterSpacing: "0px",
+  margin: "3px 4px 0 0",
+  width: "max-content",
+  borderRadius: "6px",
+  padding: "0.5rem",
+  paddingTop: "0.25rem",
+  alignItems: "center",
+  color: "pinksoft.600",
+  background: "pinksoft.200",
+}
+
 const TagCandidatureSpontanee = () => {
   return (
-    <span className="c-media-tag c-media-tag--paperplane">
-      <img src={paperplaneIcon} alt="" />
-      <span className="ml-1">Candidature spontanée</span>
-    </span>
+    <Text as="span" {...tagProperties}>
+      <Image width="16px" mb="-2px" src={paperplaneIcon} alt="" />
+      <Text whiteSpace="nowrap" as="span" ml={1}>
+        Candidature spontanée
+      </Text>
+    </Text>
   )
 }
 
