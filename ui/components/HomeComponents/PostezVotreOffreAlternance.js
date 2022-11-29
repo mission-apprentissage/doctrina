1c59b390f8fbf5f93fa9f69612950f9b30b28241
David Boureau <bdavidxyz@gmail.com>
2022-11-29 16:42:04 +0100

    feat: new component into Chakra : PostezVotreOffreAlternance

--- ui/components/HomeComponents/PostezVotreOffreAlternance.js
+++ ui/components/HomeComponents/PostezVotreOffreAlternance.js
@@ -1,22 +1,22 @@
 import React from "react"
+import { Box, Image, Text, Grid, GridItem } from "@chakra-ui/react"
 
 const PostezVotreOffreAlternance = () => {
   return (
-    <section className="p-3 mb-2 mb-md-5">
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <img className="c-homecomponent-illustration mr-3 my-3" src="/images/home_pics/illu-offreemploi.svg" alt="" />
-        </div>
-        <div className="col-12 col-md-6 pt-md-5 order-md-first">
-          <h2 className="c-homecomponent-title__small mb-3">Déposez des offres d’alternance pour vos entreprises partenaires</h2>
-          <div>
-            3 étapes seulement pour mettre en ligne les besoins de vos entreprises partenaires.
-            <br />
-            Vos offres regroupant formation et emploi seront mises en avant sur les différents sites.
-          </div>
-        </div>
-      </div>
-    </section>
+    <Box as="section">
+      <Grid>
+        <GridItem>
+          <Image src="/images/home_pics/illu-offreemploi.svg" alt=""></Image>
+        </GridItem>
+        <GridItem>
+          <Text as="h2">Déposez des offres d’alternance pour vos entreprises partenaires</Text>
+          <Box>
+            <Text>3 étapes seulement pour mettre en ligne les besoins de vos entreprises partenaires.</Text>
+            <Text>Vos offres regroupant formation et emploi seront mises en avant sur les différents sites.</Text>
+          </Box>
+        </GridItem>
+      </Grid>
+    </Box>
   )
 }
 
