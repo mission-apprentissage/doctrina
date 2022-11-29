fa109e91f9ff241898fb8719a7a613d5dfb2460c
David Boureau <bdavidxyz@gmail.com>
2022-11-29 16:44:22 +0100

    feat: chakra for GerezEntreprise

--- ui/components/HomeComponents/GerezEntreprise.js
+++ ui/components/HomeComponents/GerezEntreprise.js
@@ -1,18 +1,19 @@
 import React from "react"
+import { Box, Image, Text, Grid, GridItem } from "@chakra-ui/react"
 
 const GerezEntreprise = () => {
   return (
-    <section className="c-homecomponent__beige p-3">
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <img className="c-homecomponent-illustration mr-3 my-3" src="/images/home_pics/illu-listeoffres.svg" alt="" />
-        </div>
-        <div className="col-12 col-md-6 pt-md-5 mb-2 mb-md-5">
-          <h2 className="c-homecomponent-title__small mb-3">Gérez vos entreprises partenaires et leurs offres grâce à un tableau de bord</h2>
-          <div>Modifiez les offres, recevez les candidatures et préqualifiez les candidats pour vos entreprises et vos formations.</div>
-        </div>
-      </div>
-    </section>
+    <Box as="section">
+      <Grid>
+        <GridItem className="col-12 col-md-6">
+          <Image src="/images/home_pics/illu-listeoffres.svg" alt=""></Image>
+        </GridItem>
+        <GridItem className="col-12 col-md-6 pt-md-5 mb-2 mb-md-5">
+          <Text as="h2">Gérez vos entreprises partenaires et leurs offres grâce à un tableau de bord</Text>
+          <Text>Modifiez les offres, recevez les candidatures et préqualifiez les candidats pour vos entreprises et vos formations.</Text>
+        </GridItem>
+      </Grid>
+    </Box>
   )
 }
 
