ececb49164647cf10596a79e39c7de465a1389d4
David Boureau <bdavidxyz@gmail.com>
2022-11-29 21:06:24 +0100

    feat: sectionFollowLinkedIn

--- ui/components/HomeComponents/FollowLinkedIn.js
+++ ui/components/HomeComponents/FollowLinkedIn.js
@@ -1,8 +1,6 @@
 import React from "react"
 
-import linkedIn from "../../public/images/icons/linkedin.svg"
-import ExternalLink from "../externalLink"
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Text, Link, Image } from "@chakra-ui/react"
 
 const FollowLinkedIn = () => {
   return (
@@ -18,13 +16,14 @@ const FollowLinkedIn = () => {
         </Text>
       </Box>
       <Box display="flex" alignItems="center" justifyContent="center">
-        <ExternalLink
-          className="c-follow-linkedin-button px-3 py-2 mx-3 d-flex"
+        <Link
           url="https://www.linkedin.com/company/mission-apprentissage"
           aria-label="Accès à la page Linkedin de la mission interministérielle pour l’apprentissage et les trajectoires professionnelles"
-          title="Voir notre page &nbsp;"
-          withPic={<img src={linkedIn} alt="Lien" />}
-        />
+          title=""
+        >
+          Voir notre page &nbsp;
+          <Image src="/images/icons/linkedin.svg"></Image>
+        </Link>
       </Box>
     </Box>
   )
