96d9626d2310c85d94ea661c0224b00a9d0c68a3
David Boureau <bdavidxyz@gmail.com>
2022-11-29 20:39:43 +0100

    feat: soon

--- ui/components/HomeComponents/BientotCFA.js
+++ ui/components/HomeComponents/BientotCFA.js
@@ -6,14 +6,18 @@ const BientotCFA = () => {
   return (
     <Box>
       <Grid templateColumns={{ base: "1fr", lg: "repeat(4, 1fr)" }} templateRows={{ base: "auto", lg: "repeat(6, 1fr)" }}>
-        <GridItem gridColumn={{ base: "1fr", lg: "1 / 3" }} gridRow={{ base: "auto", lg: "1 / 4" }}>
-          <Image src="/images/home_pics/illu-offrecouplee.svg" alt="" />
+        <GridItem gridColumn={{ base: "1fr", lg: "1 / 3" }} gridRow={{ base: "auto", lg: "1 / 3" }}>
+          <Image src="/images/home_pics/illu-candidatures.svg" alt="" width={{ base: "120px", lg: "511px" }} />
         </GridItem>
-        <GridItem gridColumn={{ base: "1fr", lg: "1 / 3" }} gridRow={{ base: "auto", lg: "2 / 5" }}>
-          <Image src="/images/home_pics/illu-candidatures.svg" alt="" />
+        <GridItem gridColumn={{ base: "1fr", lg: "1 / 3" }} gridRow={{ base: "auto", lg: "3 / 5" }}>
+          <Image src="/images/home_pics/illu-offrecouplee.svg" alt="" width={{ base: "120px", lg: "511px" }} />
         </GridItem>
         <GridItem gridColumn={{ base: "1fr", lg: "3 / 5" }} gridRow={{ base: "auto", lg: "1 / 1" }}>
-          <Box>Bientôt</Box>
+          <Box display="flex" alignItems="flex-end" justifyContent="flex-start" height="100%">
+            <Text bg="linear-gradient(90deg, #6A11CB 0%, #2575FC 100%);" color="white" borderRadius={40} fontSize={20} px={4} fontWeight={700}>
+              Bientôt
+            </Text>
+          </Box>
         </GridItem>
         <GridItem gridColumn={{ base: "1fr", lg: "3 / 5" }} gridRow={{ base: "auto", lg: "2 / 2" }}>
           <Text as="h2" variant="homeEditorialH2">
