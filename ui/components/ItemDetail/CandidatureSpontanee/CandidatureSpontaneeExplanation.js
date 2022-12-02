b72fe1ed5388627fd602bc9f1f0e631348a70632
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2022-12-02 17:57:58 +0100

    Update CandidatureSpontaneeExplanation.js

--- ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeExplanation.js
+++ ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeExplanation.js
@@ -42,7 +42,7 @@ const CandidatureSpontaneeExplanation = (props) => {
                 <ExternalLink
                   className="gtmCVLink gtmClicnjob c-nice-link"
                   url="https://cv.clicnjob.fr/"
-                  title="https://cv.clicnjob.fr/"
+                  title="Accès au site Clicnjob"
                   withPic={<img src="../../images/icons/goto.svg" alt="Lien" />}
                 />
               </li>
@@ -50,7 +50,7 @@ const CandidatureSpontaneeExplanation = (props) => {
                 <ExternalLink
                   className="gtmCVLink gtmCvdesigner c-nice-link"
                   url="https://cvdesignr.com/fr"
-                  title="https://cvdesignr.com/fr"
+                  title="Accès au site CV Designer"
                   withPic={<img src="../../images/icons/goto.svg" alt="Lien" />}
                 />
               </li>
@@ -58,7 +58,7 @@ const CandidatureSpontaneeExplanation = (props) => {
                 <ExternalLink
                   className="gtmCVLink gtmCanva c-nice-link"
                   url="https://www.canva.com/fr_fr/creer/cv/"
-                  title="https://www.canva.com/fr_fr/creer/cv/"
+                  title="Accès au site Canva"
                   withPic={<img src="../../images/icons/goto.svg" alt="Lien" />}
                 />
               </li>
