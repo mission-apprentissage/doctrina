544186725cb9e2762491a664a6da8e976515e225
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2022-12-02 17:59:30 +0100

    Update LocationDetail.js

--- ui/components/ItemDetail/LocationDetail.js
+++ ui/components/ItemDetail/LocationDetail.js
@@ -201,7 +201,7 @@ const LocationDetail = ({ item, isCfa }) => {
                     <ExternalLink
                       className="c-nice-link"
                       url="https://dinum-beta.didask.com/courses/demonstration/60abc18c075edf000065c987"
-                      title="Accès au module pédagogique"
+                      title="Préparer son premier contact avec un CFA""
                       withPic={<img src={gotoIcon} alt="Ouverture dans un nouvel onglet" />}
                     />
                   </span>
