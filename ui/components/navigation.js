69e3d172ccc2257dd944c21f91637d1ea0112a72
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2022-12-07 12:22:40 +0100

    Update navigation.js

--- ui/components/navigation.js
+++ ui/components/navigation.js
@@ -85,7 +85,7 @@ const Navigation = ({ currentPage, bgcolor }) => {
                   <NavItem className="ml-lg-2">
                     <Link href="/espace-pro/authentification">
                       <a className="nav-link" aria-label="Connexion">
-                        <img className="c-homecomponent-bluelock mr-2" src="/images/icons/blue_lock.svg"/>
+                        <img className="c-homecomponent-bluelock mr-2" src="/images/icons/blue_lock.svg" alt=""/>
                         <span className="mx-1">Connexion</span>
                       </a>
                     </Link>
