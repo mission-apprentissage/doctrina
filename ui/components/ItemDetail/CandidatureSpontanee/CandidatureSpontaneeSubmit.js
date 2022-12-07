15306c31301d54cb2dd6f7546e326b65c2f734ae
Alan Le Ruyet <alan.leruyet@free.fr>
2022-12-07 11:58:47 +0100

    feat: use switch and trimming useless cases

--- ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeSubmit.js
+++ ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeSubmit.js
@@ -1,40 +1,30 @@
 import { Button, Flex, Spinner, Text } from "@chakra-ui/react"
 import React from "react"
 import { amongst } from "../../../utils/arrayutils"
-import { capitalizeFirstLetter } from "../../../utils/strutils"
 
 const CandidatureSpontaneeSubmit = (props) => {
   const sendingState = props.sendingState
   const kind = props?.item?.ideaType || ""
 
-  let res = ""
-  if (sendingState === "not_sent") {
-    res = (
-      <Button aria-label="je-postule" variant="blackButton" type="submit" data-testId="candidature-not-sent">
-        {amongst(kind, ["lbb", "lba"]) ? "J'envoie ma candidature spontanée" : "J'envoie ma candidature"}
-      </Button>
-    )
-  } else if (sendingState === "ok_sent") {
-    res = (
-      <Text as="span" data-testId="candidature-submit-ok">
-        Succès
-      </Text>
-    )
-  } else if (sendingState === "currently_sending") {
-    res = (
-      <Flex alignItems="center" direction="row" data-testId="candidature-currently-sending">
-        <Spinner mr={4} />
-        <Text>Veuillez patienter</Text>
-      </Flex>
-    )
-  } else if (sendingState === "not_sent_because_of_errors") {
-    res = (
-      <Text fontWeight={700} color="red.500" as="span" data-testId="candidature-submit-error">
-        ⚠ Erreur lors de l&apos;envoi, veuillez réessayer plus tard
-      </Text>
-    )
+  switch (sendingState) {
+    case "not_sent": {
+      return (
+        <Button aria-label="je-postule" variant="blackButton" type="submit" data-testid="candidature-not-sent">
+          {amongst(kind, ["lbb", "lba"]) ? "J'envoie ma candidature spontanée" : "J'envoie ma candidature"}
+        </Button>
+      )
+    }
+    case "currently_sending": {
+      return (
+        <Flex alignItems="center" direction="row" data-testid="candidature-currently-sending">
+          <Spinner mr={4} />
+          <Text>Veuillez patienter</Text>
+        </Flex>
+      )
+    }
+    default:
+      return ""
   }
-  return res
 }
 
 export default CandidatureSpontaneeSubmit
