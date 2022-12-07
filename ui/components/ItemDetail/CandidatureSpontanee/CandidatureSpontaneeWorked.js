1d81c5b9fe153527c41016368faa5649951ddde4
Alan Le Ruyet <alan.leruyet@free.fr>
2022-12-07 11:48:52 +0100

    fix: use Container instead of ModalBody to avoid context error

--- ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeWorked.js
+++ ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeWorked.js
@@ -1,11 +1,11 @@
-import { Box, Flex, Image, ModalBody, Text } from "@chakra-ui/react"
+import { Box, Container, Flex, Image, Text } from "@chakra-ui/react"
 import React from "react"
 import paperplaneIcon from "../../../public/images/paperplane2.svg"
 import { testingParameters } from "../../../utils/testingParameters"
 
 const CandidatureSpontaneeWorked = ({ email, company, kind }) => {
   return (
-    <ModalBody data-testid="CandidatureSpontaneeWorked">
+    <Container data-testid="CandidatureSpontaneeWorked">
       <Text as="h1" fontSize="24px" fontWeight={700} data-testid="CandidatureSpontaneeWorkedTitle">
         {kind === "matcha" ? <>Postuler à l&apos;offre de {company}</> : <>Candidature spontanée</>}
       </Text>
@@ -31,7 +31,7 @@ const CandidatureSpontaneeWorked = ({ email, company, kind }) => {
       <Text fontSize="18px" mt={4} mb={12}>
         Si vous n&apos;avez pas reçu d&apos;email de confirmation d&apos;ici 24 heures, soumettez à nouveau votre candidature
       </Text>
-    </ModalBody>
+    </Container>
   )
 }
 
