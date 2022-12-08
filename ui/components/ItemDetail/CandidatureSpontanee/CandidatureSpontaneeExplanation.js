de91b2b103e3acdf41fd22681ab4d3cf2899e6a8
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2022-12-08 15:57:00 +0100

    Fix - paramètres rgaa externallink

--- ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeExplanation.js
+++ ui/components/ItemDetail/CandidatureSpontanee/CandidatureSpontaneeExplanation.js
@@ -42,7 +42,6 @@ const CandidatureSpontaneeExplanation = (props) => {
                 <ExternalLink
                   className="gtmCVLink gtmClicnjob c-nice-link"
                   url="https://cv.clicnjob.fr/"
-                  title="Lien vers le site Clicnjob"
                   withPic={<img src="../../images/icons/goto.svg" alt="Lien" />}
                 />
               </li>
@@ -50,7 +49,6 @@ const CandidatureSpontaneeExplanation = (props) => {
                 <ExternalLink
                   className="gtmCVLink gtmCvdesigner c-nice-link"
                   url="https://cvdesignr.com/fr"
-                  title="Lien vers le site CV Designer"
                   withPic={<img src="../../images/icons/goto.svg" alt="Lien" />}
                 />
               </li>
@@ -58,7 +56,6 @@ const CandidatureSpontaneeExplanation = (props) => {
                 <ExternalLink
                   className="gtmCVLink gtmCanva c-nice-link"
                   url="https://www.canva.com/fr_fr/creer/cv/"
-                  title="Lien vers le site Canva"
                   withPic={<img src="../../images/icons/goto.svg" alt="Lien" />}
                 />
               </li>
