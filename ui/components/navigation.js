a1137af4a1fde50b2ce5909597a6a1ef048ebf63
David Boureau <bdavidxyz@gmail.com>
2022-12-12 11:03:23 +0100

    feat: hamburger icon

--- ui/components/navigation.js
+++ ui/components/navigation.js
@@ -1,13 +1,11 @@
 import { useRouter } from "next/router"
 import { useState } from "react"
-import { Box, Image, Link } from "@chakra-ui/react"
+import { Box, Container, Flex, Image, Link, useDisclosure, Stack, Show } from "@chakra-ui/react"
 
-const Navigation = ({ currentPage, bgcolor }) => {
+import { HamburgerIcon } from "@chakra-ui/icons"
+
+const Navigation = ({ currentPage, bgcolor, ...props }) => {
   const router = useRouter()
-
-  const [isOpen, setIsOpen] = useState(false)
-
-  const toggle = () => setIsOpen(!isOpen)
 
   let main_class_name = "c-navigation "
   main_class_name += bgcolor ?? "is-filled"
@@ -33,7 +31,44 @@ const Navigation = ({ currentPage, bgcolor }) => {
     return url
   }
 
-  return <Box></Box>
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <Box>
+      <Flex py={2} px={4} direction={["column", "row"]} justify="space-between" bg="orange.400">
+        <Flex alignItems="center" wrap="wrap">
+          <Flex flexGrow={1} justify="center">
+            <Box ml={[4, 0]}>
+              <Image src="/images/marianne.svg#svgView(viewBox(12 0 162 78))" alt="" width="162" height="78" />
+              <Show above="lg">
+                <Image src={`/images/${getLogo()}`} alt="Redirection vers la page d'accueil" width="110" height="76" />
+              </Show>
+            </Box>
+          </Flex>
+          <HamburgerIcon onClick={isOpen ? onClose : onOpen} display={["inline", "none"]} />
+        </Flex>
+        <Flex display={[isOpen ? "flex" : "none", "flex"]} bg={["teal.500", "orange.400"]}>
+          <Stack align="center" direction={["column", "row"]}>
+            <Link href="/" aria-label="Accès espace candidat">
+              <Box as="span" mx="1">
+                Candidat
+              </Box>
+            </Link>
+            <Link href="/acces-recruteur" aria-label="Accès espace recruteur">
+              <Box as="span" mx="1">
+                Recruteur
+              </Box>
+            </Link>
+            <Link href="/organisme-de-formation">
+              <a className="nav-link" aria-label="Accès espace organisme de formation">
+                <span className="mx-1">Organisme de formation</span>
+              </a>
+            </Link>
+          </Stack>
+        </Flex>
+      </Flex>
+    </Box>
+  )
 }
 
 export default Navigation
