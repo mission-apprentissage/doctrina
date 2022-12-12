fb872023dfb41eed946f4e16bbe626f89b7337dd
David Boureau <bdavidxyz@gmail.com>
2022-12-12 11:28:06 +0100

    style: container

--- ui/components/navigation.js
+++ ui/components/navigation.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router"
-import { Box, Flex, Image, Link, useDisclosure, Stack, Show } from "@chakra-ui/react"
+import { Box, Flex, Image, Link, useDisclosure, Stack, Show, Container } from "@chakra-ui/react"
 
 import { HamburgerIcon } from "@chakra-ui/icons"
 
@@ -34,38 +34,40 @@ const Navigation = ({ currentPage, bgcolor, ...props }) => {
 
   return (
     <Box>
-      <Flex py={2} px={4} direction={["column", "column", "row"]} justify="space-between" bg="orange.400">
-        <Flex alignItems="center" wrap="wrap">
-          <Flex flexGrow={1}>
-            <Box ml={[4, 4, 0]} display="flex">
-              <Image src="/images/marianne.svg#svgView(viewBox(12 0 162 78))" alt="" width="162" height="78" />
-              <Show above="lg">
-                <Image src={`/images/${getLogo()}`} alt="Redirection vers la page d'accueil" width="110" height="76" />
-              </Show>
-            </Box>
+      <Container variant="responsiveContainer">
+        <Flex py={2} px={4} direction={["column", "column", "row"]} justify="space-between" bg="orange.400">
+          <Flex alignItems="center" wrap="wrap">
+            <Flex flexGrow={1}>
+              <Box ml={[4, 4, 0]} display="flex">
+                <Image src="/images/marianne.svg#svgView(viewBox(12 0 162 78))" alt="" width="162" height="78" />
+                <Show above="lg">
+                  <Image src={`/images/${getLogo()}`} alt="Redirection vers la page d'accueil" width="110" height="76" />
+                </Show>
+              </Box>
+            </Flex>
+            <HamburgerIcon onClick={isOpen ? onClose : onOpen} display={["inline", "inline", "none"]} cursor="pointer" />
           </Flex>
-          <HamburgerIcon onClick={isOpen ? onClose : onOpen} display={["inline", "inline", "none"]} cursor="pointer" />
+          <Flex display={[isOpen ? "flex" : "none", isOpen ? "flex" : "none", "flex"]}>
+            <Stack align="left" direction={["column", "column", "row"]}>
+              <Link href="/" aria-label="Accès espace candidat">
+                <Box as="span" mx="1">
+                  Candidat
+                </Box>
+              </Link>
+              <Link href="/acces-recruteur" aria-label="Accès espace recruteur">
+                <Box as="span" mx="1">
+                  Recruteur
+                </Box>
+              </Link>
+              <Link href="/organisme-de-formation" aria-label="Accès espace organisme de formation">
+                <Box as="span" mx="1">
+                  Organisme de formation
+                </Box>
+              </Link>
+            </Stack>
+          </Flex>
         </Flex>
-        <Flex display={[isOpen ? "flex" : "none", isOpen ? "flex" : "none", "flex"]}>
-          <Stack align="left" direction={["column", "column", "row"]}>
-            <Link href="/" aria-label="Accès espace candidat">
-              <Box as="span" mx="1">
-                Candidat
-              </Box>
-            </Link>
-            <Link href="/acces-recruteur" aria-label="Accès espace recruteur">
-              <Box as="span" mx="1">
-                Recruteur
-              </Box>
-            </Link>
-            <Link href="/organisme-de-formation" aria-label="Accès espace organisme de formation">
-              <Box as="span" mx="1">
-                Organisme de formation
-              </Box>
-            </Link>
-          </Stack>
-        </Flex>
-      </Flex>
+      </Container>
     </Box>
   )
 }
