0395fe7b8844334bd890a26a0fad8bccc1305046
Kevin Barnoin <k.barnoin@gmail.com>
2023-01-05 15:40:59 +0100

    feat: update job

--- server/src/jobs/lba_recruteur/formulaire/misc/getOpcofromSiret.js
+++ server/src/jobs/lba_recruteur/formulaire/misc/getOpcofromSiret.js
@@ -1,4 +1,3 @@
-import axios from "axios"
 import { logger } from "../../../../common/logger.js"
 import { Formulaire } from "../../../../common/model/index.js"
 import { asyncForEach } from "../../../../common/utils/asyncUtils.js"
@@ -6,30 +5,47 @@ import { runScript } from "../../../scriptWrapper.js"
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
-runScript(async () => {
+runScript(async ({ etablissementsRecruteur }) => {
   const form = await Formulaire.find({ $or: [{ opco: { $exists: false } }, { opco: null }] })
   logger.info(`${form.length} entreprise à rechercher`)
-  let count = 0
+  let stat = { found: 0, multiple: 0, notFount: 0 }
 
   await asyncForEach(form, async (f) => {
-    logger.info(`get idcc for ${f.siret}`)
-    const idcc = await axios.get(`https://siret2idcc.fabrique.social.gouv.fr/api/v2/${f.siret}`)
+    let opcoResult = await etablissementsRecruteur.getOpco(f.siret)
 
-    if (idcc.data[0].conventions?.length === 0) {
-      logger.info("Not Found")
-      return
+    switch (opcoResult.data?.searchStatus) {
+      case "OK":
+        f.opco = opcoResult.data?.opcoName
+        f.idcc = opcoResult.data?.idcc
+        stat.found++
+        break
+
+      case "MULTIPLE_OPCO":
+        f.opco = "Opco multiple"
+        f.idcc = "Opco multiple, IDCC non définit"
+        stat.multiple++
+        break
+
+      case "NOT_FOUND":
+        opcoResult = await etablissementsRecruteur.getIdcc(req.params.siret)
+        if (opcoResult.data[0].conventions?.length !== 0) {
+          const { num } = opcoResult.data[0]?.conventions[0]
+          opcoResult = await etablissementsRecruteur.getOpcoByIdcc(num)
+
+          f.opco = opcoResult.data?.opcoName ?? undefined
+          f.idcc = opcoResult.data?.idcc ?? undefined
+          return
+        }
+        stat.notFount++
+        break
+
+      default:
+        break
     }
-
-    const { num } = idcc.data[0]?.conventions[0]
-
-    const { data } = await axios.get(`https://www.cfadock.fr/api/opcos?idcc=${num}`)
-    logger.info(`Opco: ${data.opcoName} - Idcc: ${data.idcc} –-> siret: ${f.siret} — ${f.email}`)
-    f.opco = data.opcoName
-    f.idcc = data.idcc
 
     await f.save()
     count++
-    await delay(2000)
+    await delay(900)
   })
 
   return { count }
