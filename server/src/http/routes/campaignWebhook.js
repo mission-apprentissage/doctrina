8f8f3992ea5b1eda3db1390d3817bb7b99829b78
Alan Le Ruyet <alan.leruyet@free.fr>
2023-01-11 14:59:33 +0100

    feat: route et service

---
+++ server/src/http/routes/campaignWebhook.js
@@ -0,0 +1,15 @@
+import express from "express"
+import { updateHardBounceEmails } from "../../service/applications.js"
+import { tryCatch } from "../middlewares/tryCatchMiddleware.js"
+
+export default function (components) {
+  const router = express.Router()
+
+  router.post(
+    "/webhook",
+    tryCatch(async (req, res) => {
+      updateHardBounceEmails({ payload: req.body, ...components })
+      return res.json({ result: "ok" })
+    })
+  )
+}
